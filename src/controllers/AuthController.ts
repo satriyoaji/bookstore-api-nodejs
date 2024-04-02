e1a890da24efc2191244d755df0239b4c6c4708f
Muhammad Satriyo Aji <satriyoaji27@gmail.com>
2024-04-02 23:12:51 +0700

    delete password payload on auth

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,6 +5,7 @@ export const auth = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
     const { customer, token } = await authService.auth(username, password);
+    customer.password = ""
     res.json({ customer, token });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
